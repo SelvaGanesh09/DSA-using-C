1fa860170fc6986adb1b4904af77a5cb73605191
selvaganeshgaming-dotcom <selvaganesh.gaming@gmail.com>
2026-04-03 17:57:23 +0530

    Edited on 03.04.2026

--- Data_Structures/03_Strings/09_basic.c
+++ Data_Structures/03_Strings/09_basic.c
@@ -1,1 +1,22 @@
-
+//Write a program to convert the Upper case character string to Lower case
+#include<stdio.h>
+void toLower(char*);
+int main()
+{
+	char str[100]="EducATion is a FestiVAl";
+	toLower(str);
+	puts(str);
+	return 0;
+}
+void toLower(char *str)
+{
+	int i=0;
+	while(str[i]!='\0')
+	{
+		if(str[i]>=65 && str[i]<=90)
+		{
+			str[i]=str[i]+32;
+		}
+		i++;
+	}
+}
